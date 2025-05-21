2df6c1a3dec5d10a66ccb58bec0f29229389ce53
Md Abu Rayhan <mdaburayhan833@gmail.com>
2025-05-21 17:30:51 +0600

    create MyViewPagerAdapter and connect the Addapter to the viewPager using the MainActivity.java

--- app/src/main/java/com/arsoft/viewpagerapp/MainActivity.java
+++ app/src/main/java/com/arsoft/viewpagerapp/MainActivity.java
@@ -7,18 +7,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager2 viewPager;
+    MyViewPagerAdapter myViewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        viewPager = findViewById(R.id.viewPager2);
+
+        myViewPagerAdapter = new MyViewPagerAdapter(
+                getSupportFragmentManager(),
+                getLifecycle()
+        );
+
+        // Adding Fragments to the list in the Adapter Class
+        myViewPagerAdapter.addFragment(new Fragment1());
+        myViewPagerAdapter.addFragment(new Fragment2());
+        myViewPagerAdapter.addFragment(new Fragment3());
+
+        // Set the orientation in ViewPager2
+        viewPager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+
+        // Connection the Adapter with the ViewPager2
+        viewPager.setAdapter(myViewPagerAdapter);
+
+
+
+
     }
 }
--- app/src/main/java/com/arsoft/viewpagerapp/MyViewPagerAdapter.java
+++ app/src/main/java/com/arsoft/viewpagerapp/MyViewPagerAdapter.java
@@ -7,6 +7,34 @@ import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.lifecycle.Lifecycle;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
-public class MyViewPagerAdapter {
+import java.util.ArrayList;
+
+public class MyViewPagerAdapter extends FragmentStateAdapter {
+
+    private ArrayList<Fragment> fragmentList = new ArrayList<>();
+
+    public MyViewPagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        // responsible for creating and returning a
+        // fragment for a specific position within ViewPager2
+
+
+
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return fragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment){
+        fragmentList.add(fragment);
+    }
 
 }
