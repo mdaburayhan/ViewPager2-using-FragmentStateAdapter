963541c09c85331daee2212ba0d38556262b1833
Md Abu Rayhan <mdaburayhan833@gmail.com>
2025-05-21 17:17:44 +0600

    3 Fragment Created and the ViewPager2 widget is initialized

---
+++ app/src/main/java/com/arsoft/viewpagerapp/MyViewPagerAdapter.java
@@ -0,0 +1,12 @@
+package com.arsoft.viewpagerapp;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class MyViewPagerAdapter {
+
+}
