fe745c13d343648f6e7e803d1643c2fab13ff42a
Md Abu Rayhan <mdaburayhan833@gmail.com>
2025-05-21 17:49:37 +0600

    connecting the ViewPager2 to the tabLayout

--- app/src/main/java/com/arsoft/viewpagerapp/MainActivity.java
+++ app/src/main/java/com/arsoft/viewpagerapp/MainActivity.java
@@ -3,16 +3,21 @@ package com.arsoft.viewpagerapp;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 public class MainActivity extends AppCompatActivity {
 
     ViewPager2 viewPager;
     MyViewPagerAdapter myViewPagerAdapter;
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         viewPager = findViewById(R.id.viewPager2);
+        tabLayout = findViewById(R.id.tabLayout);
 
         myViewPagerAdapter = new MyViewPagerAdapter(
                 getSupportFragmentManager(),
@@ -36,6 +42,19 @@ public class MainActivity extends AppCompatActivity {
 
         // Connection the Adapter with the ViewPager2
         viewPager.setAdapter(myViewPagerAdapter);
+
+
+        // Connecting TabLayout with ViewPager
+        new TabLayoutMediator(
+                tabLayout,
+                viewPager,
+                new TabLayoutMediator.TabConfigurationStrategy() {
+                    @Override
+                    public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                        tab.setText("Fragment "+(position+1));
+                    }
+                }
+        ).attach();
 
 
 
